112d070d145de2731f4ab5d80123c03a8ab07feb
matthew <g00430349@atu.ie>
2025-10-02 21:20:57 +0100

    added validation and a list

--- src/main/java/com/example/demo/ProductController.java
+++ src/main/java/com/example/demo/ProductController.java
@@ -1,18 +1,26 @@
 package com.example.demo;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import jakarta.validation.Valid;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @RequestMapping("/product")
 @RestController
 public class ProductController {
+    List<product> mylist = new ArrayList<product>();
     @GetMapping("/hello")
     public String hello() {
         return "Hello World";
     }
     @GetMapping("/getproduct")
     public product getproduct() {
-        product myproduct = new product("tv",499);
+        product myproduct = new product("tv",500);
+        return myproduct;
+    }
+    @PostMapping("/addproduct")
+    public product addproduct(@Valid @RequestBody product myproduct) {
+        mylist.add(myproduct);
         return myproduct;
     }
 }
--- src/main/java/com/example/demo/product.java
+++ src/main/java/com/example/demo/product.java
@@ -1,9 +1,13 @@
 package com.example.demo;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Positive;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 @AllArgsConstructor
 @Data
 public class product {
+    @NotBlank
     private String productname;
-    private int productprice;
+    @Positive
+    private double productprice;
 }
