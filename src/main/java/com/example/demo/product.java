47647ba761555cfb7fd79d8ca786ec382f0d8801
matthew <g00430349@atu.ie>
2025-10-02 21:28:46 +0100

    added a count for product and 2 validations

--- src/main/java/com/example/demo/product.java
+++ src/main/java/com/example/demo/product.java
@@ -1,13 +1,17 @@
 package com.example.demo;
+import jakarta.validation.constraints.DecimalMin;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Positive;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 @AllArgsConstructor
 @Data
 public class product {
     @NotBlank
+    @Size(min = 2, max = 12)
     private String productname;
     @Positive
+    @DecimalMin(value = "0.10")
     private double productprice;
 }
